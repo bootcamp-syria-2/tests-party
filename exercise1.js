48bfd8786a4071272d02bc57e318bec2e47e4cc4
Rafael Alegre <51097035+cabecasd@users.noreply.github.com>
2024-10-15 12:32:44 +0100

    Update exercise1.js

--- exercise1.js
+++ exercise1.js
@@ -1,10 +1,12 @@
+//Code written by the Junior Developer Mike
 function filterAndSort1(items, minValue) {
 
     return items
       .filter(item => item.value >= minValue)
       .sort((a, b) => a.value - b.value);
 }
-  
+
+//Code written by the Junior Developer John
 function filterAndSort2(items, minValue) {
 
     return items
@@ -12,6 +14,7 @@ function filterAndSort2(items, minValue) {
       .sort((a, b) => b.value - a.value);     
 }
 
+//Code written by the Junior Developer Martha
 function filterAndSort3(items, minValue) {
 
     return items
