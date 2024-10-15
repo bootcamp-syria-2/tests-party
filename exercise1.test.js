3be092333520a3cfd7a4ac644c79aa8cbfdcadc8
Rafael Alegre <51097035+cabecasd@users.noreply.github.com>
2024-10-15 12:34:45 +0100

    Update exercise1.test.js

--- exercise1.test.js
+++ exercise1.test.js
@@ -1,12 +1,14 @@
 const { filterAndSort1, filterAndSort2, filterAndSort3 } = require("./exercise1.js");
 
-const input = [    
+//The values `data` and `min` are the inputs to test the Junior Developer's code with.
+const data = [    
     { name: "Product 1", value: 50 },
     { name: "Product 2", value: 30 },
     { name: "Product 3", value: 80 }
 ]
 const min = 40
 
+//The expected (correct) results when running the functions
 const expectedResults = [
     {
         "name": "Product 1",
